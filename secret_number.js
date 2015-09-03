5198961d519751daeb9d5d1c9adb3aadaa0912b5
styel07 <eleazarcorpuz@gmail.com>
2015-09-02 17:01:52 -1000

    finished generating secret number

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,7 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = (function() {
+  var secretNumber = Math.floor((Math.random() * 1000000) + 1);
+  return function() {
+    return secretNumber;
+  };
+});
